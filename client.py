e1867fd74fffa3ceddb6bb145e5929580e7dda31
szepema <61192957+szepema@users.noreply.github.com>
2023-04-29 23:08:13 +0200

    Finished with main goals

--- client.py
+++ client.py
@@ -5,9 +5,12 @@ import random
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
 
+
 context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
 
 context.load_verify_locations('server.crt')
+
+balance = 100000
 
 
 # Connect to server
@@ -17,8 +20,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
     with context.wrap_socket(sock, server_hostname='localhost') as ssock:
         
         ssock.connect(('localhost', 8000))
-        
-        
+                
         # Receive public key from server
         
         public_key = ssock.recv(1024)
@@ -47,57 +49,69 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
                 
                 ssock.send(encrypted_password)
                 
+            elif 'two-factor' in response.lower():
+                
+                two_factor = input(response)
+                               
+                encrypted_two_factor = cipher.encrypt(two_factor.encode())
+                
+                ssock.send(encrypted_two_factor)
+                
             else:
                 
                 print(response)
                 
+                
             if response.startswith('Login successful'):
+                
+                fuel_amount = random.randint(10, 75)
+                
+                fuel_type = random.randint(1, 4)
+                
+                type_dict = {
+                    1:"95 Petrol",
+                    2:"Diesel",
+                    3:"Premium Petrol",
+                    4:"Premium Diesel",
+                }
+                
+                fuel_type_str = type_dict.get(fuel_type, "Invalid number")
+                
+                print('You filled up with a total of {} litres.'
+                ' Fuel type: {}'.format(fuel_amount, fuel_type_str)) 
+                
+                # Encrypting and sending fueling data
+                
+                amount_bytes = fuel_amount.to_bytes((fuel_amount.bit_length() + 7) // 8, byteorder='big')
+                
+                encrypted_fuel_amount = cipher.encrypt(amount_bytes)  
+                
+                ssock.send(encrypted_fuel_amount)
+                
+                type_bytes = fuel_type.to_bytes((fuel_type.bit_length() + 7) // 8, byteorder='big')  
+               
+                encrypted_fuel_type = cipher.encrypt(type_bytes)  
+                
+                ssock.send(encrypted_fuel_type)  
+                
+                # Receving and decrypting the price
+
+                bytes_fuel_price = ssock.recv(1024)
+                
+                int_fuel_price = int.from_bytes(bytes_fuel_price, byteorder='big')
+                
+                print('Cost of the fuel:{}'.format(int_fuel_price)) 
+                
+                new_balance = balance - int_fuel_price
+                
+                print('Fuel is paid for, new balance is: {}'.format(new_balance)) 
                 
                 break
             
             elif response.startswith('Error'):
                 
-                print('Incorrect credentials')
-
                 break
-                
-        # Determine fuel type and amount
              
-        fuel_amount = random.randint(10, 75)
-        
-        fuel_type = random.randint(1, 4)
-        
-        type_dict = {
-            1:"95 Benzin",
-            2:"Diesel",
-            3:"Prémium Benzin",
-            4:"Prémium Diesel",
-        }
-        
-        fuel_type_str = type_dict.get(fuel_type, "Invalid number")
-        
-        print('Tankolásnál összesen {} litert tankoltál.'
-        ' Üzemanyag típusa: {}'.format(fuel_amount, fuel_type_str)) 
-        
-        # Encrypting and sending fueling data
-        
-        amount_bytes = fuel_amount.to_bytes((fuel_amount.bit_length() + 7) // 8, byteorder='big')
-        
-        encrypted_fuel_amount = cipher.encrypt(amount_bytes)  
-        
-        ssock.send(encrypted_fuel_amount)
-        
-        type_bytes = fuel_type.to_bytes((fuel_type.bit_length() + 7) // 8, byteorder='big')  
-       
-        encrypted_fuel_type = cipher.encrypt(type_bytes)  
-        
-        ssock.send(encrypted_fuel_type)  
-        
-        # Receving and decrypting the price
 
-        # fuel_price = ssock.recv(1024)            
-        
-        # print(fuel_price)
-        
         
             
--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import socket
 import ssl
+import random
 
 from Crypto.PublicKey import RSA
 from Crypto.Cipher import PKCS1_OAEP
@@ -8,8 +9,8 @@ from Crypto.Cipher import PKCS1_OAEP
 
 users = {'client1': 'password123', 'client2': 'password456'}
 
-fuel_price = {'95 Benzin': 610,'Prémium Diesel': 640,
-              'Diesel': 585,'Prémium Benzin': 647 }
+fuel_price = {'95 Petrol': 610,'Premium Diesel': 640,
+              'Diesel': 585,'Premium Petrol': 647 }
 
 # Generate and import key pair
 
@@ -66,50 +67,67 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM, 0) as sock:
              
              encrypted_password = ssock.recv(1024)
                        
-             decrypted_password = cipher.decrypt(encrypted_password).decode("utf-8")          
+             decrypted_password = cipher.decrypt(encrypted_password).decode("utf-8") 
+             
+             two_factor = random.randint(1000, 9999)
+             
+             str_two_factor = str(two_factor)
+                
+             ssock.sendall(('The two-factor verification code is '
+                        + str_two_factor +'Please enter the code:').encode()) 
+                
+             encrypted_verification = ssock.recv(1024)
+                 
+             decrypted_verification = cipher.decrypt(encrypted_verification).decode("utf-8")
+             
              
              # Verify the credentials against the stored values
              
-             if decrypted_username in users and users[decrypted_username] == decrypted_password:
+             if decrypted_username in users and users[decrypted_username] == decrypted_password and decrypted_verification == str_two_factor:
                  
                  ssock.send('Login successful!'.encode())
+                 
+                 # Recive the fuel data, calculate cost and send it back to client
+                 
+                 encrypted_fuel_amount = ssock.recv(1024)
+                 
+                 decrypted_amount_bytes = cipher.decrypt(encrypted_fuel_amount)
+                 
+                 decrypted_fuel_amount = int.from_bytes(decrypted_amount_bytes, byteorder='big')
+                 
+                 encrypted_fuel_type = ssock.recv(1024)
+                            
+                 decrypted_type_bytes = cipher.decrypt(encrypted_fuel_type)
+                 
+                 decrypted_fuel_type = int.from_bytes(decrypted_type_bytes, byteorder='big')
+                 
+                 
+                 type_dict = {
+                     1:"95 Petrol",
+                     2:"Diesel",
+                     3:"Premium Petrol",
+                     4:"Premium Diesel",
+                 }
+                 
+                 fuel_type_str = type_dict.get(decrypted_fuel_type, "Invalid number")
+                 
+                 fuel_price = fuel_price[fuel_type_str]
+                 
+                 final_price = fuel_price * decrypted_fuel_amount
+                 
+                 print(final_price)
+                 
+                 price_bytes = final_price.to_bytes((final_price.bit_length() + 7) // 8, byteorder='big')
+                 
+                 ssock.sendall(price_bytes)
+                 
                  
              else:
                  
                  ssock.send('Error: Incorrect username or password.'.encode())
                  
-             # Recive the fuel data, calculate cost and send it back to client
-             
-             encrypted_fuel_amount = ssock.recv(1024)
-             
-             decrypted_amount_bytes = cipher.decrypt(encrypted_fuel_amount)
-             
-             decrypted_fuel_amount = int.from_bytes(decrypted_amount_bytes, byteorder='big')
-             
-             encrypted_fuel_type = ssock.recv(1024)
-                        
-             decrypted_type_bytes = cipher.decrypt(encrypted_fuel_type)
-             
-             decrypted_fuel_type = int.from_bytes(decrypted_type_bytes, byteorder='big')
-             
-             
-             type_dict = {
-                 1:"95 Benzin",
-                 2:"Diesel",
-                 3:"Prémium Benzin",
-                 4:"Prémium Diesel",
-                 5:"Autógáz"
-             }
-             
-             fuel_type_str = type_dict.get(decrypted_fuel_type, "Invalid number")
-             
-             fuel_price = fuel_price[fuel_type_str]
-             
-             final_price = fuel_price * decrypted_fuel_amount
-             
-             # print(final_price)
-             
-             # ssock.send(final_price)
-             
+                 break
+                 
+
      except BlockingIOError:
          pass  # no client connection yet, keep waiting
